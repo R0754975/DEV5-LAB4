33714a6ad267cfa7c1d8f6b5508113fde658a41d
R0754975 <r0754975@student.thomasmore.be>
2022-10-25 14:10:50 +0200

    feat(main): added scene + helpers, created basic plane to use as ground

--- main.js
+++ main.js
@@ -1,23 +1,57 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const scene = new THREE.Scene();
 
-setupCounter(document.querySelector('#counter'))
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+
+// allow orbit controls
+const controls = new OrbitControls(camera, renderer.domElement);
+
+// render the scene
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+
+// lighting
+const light = new THREE.AmbientLight( 0x505050, 5 ); // soft white light
+scene.add( light );
+
+const light1 = new THREE.DirectionalLight( 0xbbbbff, 1 );
+light1.position.set( 3, 3, 1 );
+scene.add( light1 );
+
+// helpers
+const gridHelper = new THREE.GridHelper( 50, 50 );
+scene.add( gridHelper ); 
+
+const helper = new THREE.DirectionalLightHelper( light1 , 2);
+scene.add( helper );
+
+
+// basic ground of the scene
+const geometry = new THREE.PlaneGeometry( 20, 20 );
+			const material = new THREE.MeshLambertMaterial( { color: 0xffffff } );
+			const plane = new THREE.Mesh( geometry, material );
+      // use doubleside otherwise only one side will be rendered
+      plane.material.side = THREE.DoubleSide;
+      plane.rotation.x = Math.PI / 2;
+			scene.add( plane );
+      camera.position.z = 5;
+      camera.position.y = 20;
+      camera.lookAt(plane.position);
+
+
+// animate certain things      
+function animate() {
+				requestAnimationFrame( animate );
+
+
+				renderer.render( scene, camera );
+};
+
+
+animate();
